92c5616fea2bd41c41c4207781e694c8de7dc531
ArthurOReis <arthuroreis2005@gmail.com>
2023-05-18 08:34:54 -0300

    Adicionando imagem.html

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from galeria.views import * #De views, em galeria, importar todos os métodos
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="index"),
+    path('imagem/', imagem, name="imagem"),
 ]
